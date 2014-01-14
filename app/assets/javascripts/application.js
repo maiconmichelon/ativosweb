af7480c449d30c71fd6d49c8eeef2ff90697383b
maicon.fermo@gmail.com <maicon.fermo@gmail.com>
2014-01-13 23:08:08 -0200

    manytomany

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,12 +21,11 @@
 
 function remove_fields (link) {
   $(link).prev("input[type=hidden]:first").val('1');
-    $(link).closest(".fields").hide();
-
+  $(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).parent().before(content.replace(regexp, new_id));
+  $(link).parent().parent().before(content.replace(regexp, new_id));
 }
