85f46bbdb1d7082c00e3939d5c8b6e030cc33e2d
Maicon Michelon <maicon.f.michelon@gmail.com>
2014-06-21 16:43:27 -0300

    inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,11 +1,13 @@
+# encoding: UTF-8
+
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.plural 'Manutenção', 'Manutenções'
   inflect.plural 'Fornecedor', 'Fornecedores'
+  inflect.plural 'Manutenção', 'Manutenções'
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
